8a41bcc4e70aefe42bb5ebfe3a71b42c2e5503b9
Guest <guest-yffgbq@h26817.sgb.skir.kr>
2021-02-21 23:35:53 +0900

    I need constration

--- main.js
+++ main.js
@@ -1,17 +1,19 @@
-https://www.bsidesoft.com/285
+//https://www.bsidesoft.com/285
 class Test{
     constructor(){
         this.test = document.getElementById( "test" );
         this.logCount = 0;
         
-        this.test.addEventListener( 'touchstart', this.touchListener.bind(this,event) );
-        this.test.addEventListener( 'touchend', this.touchListener.bind(this,event) );
+        this.test.addEventListener( 'touchstart', this.touchListener,false );
+        this.test.addEventListener( 'touchend', this.touchListener.bind(this,event),false );
     }
     touchListener(e){
         let i, j, t0, touch;
        
         //touches부터 정리
         touch = e.touches;
+        console.log(touch);
+    
         t0 = [ '<b>',
           e.type,
           '</b><br>touches(length:',
@@ -26,7 +28,7 @@ class Test{
           );
        
         //changedTouches 정리
-        touch = $e.changedTouches;
+        touch = e.changedTouches;
         t0.push(
           ')<br> changed(length:',
           j =touch.length
@@ -39,18 +41,19 @@ class Test{
           );
        
         //로그에 보고
-        log( t0.join( '' ) + ')' );
+        this.log( t0.join( '' ) + ')' );
         test.dispatchEvent( new Event("to") );
     }
 
     log( $v ){
-        if( ++logCount > 10 ){
-          logCount = 0;
-          test.innerHTML = $v + "<br>";
+        if( ++this.logCount > 10 ){
+          this.logCount = 0;
+          this.test.innerHTML = $v + "<br>";
         }else{
           test.innerHTML += $v + "<br>";
         }
     }
+    
 }
 
 window.addEventListener('load', ()=>{
